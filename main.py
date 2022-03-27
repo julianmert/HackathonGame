f5e68eef3326891e68fa6781574a64a6ffa94169
paulnel-zutari <75248281+paulnel-zutari@users.noreply.github.com>
2022-03-27 12:38:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from game import Game
 
 if __name__=="__main__":
     pokemon_list_path = r'pokemon_list.csv'
-    pokemone_game = Game(pokemon_list_path)
-    pokemone_game.run()
+    pokemon_game = Game(pokemon_list_path)
+    pokemon_game.run()
